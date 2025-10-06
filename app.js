8329d79d8a9dd76842386650899d54d2b86cd228
Mosi <solgimostafa91@gmail.com>
2025-10-06 17:40:10 +0200

    change chef

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 console.log("mostafa is the best person ever");
 
+const chef = "mostafa solgi 
+
+chef.foreach( agha => {
+   
+ agha.addEventlistener('click',()=>{  
+
+   agha.innertext="oyeee"
+
+ }}
+  
+
+function oyee (loch){
+
+   loch.innerHTML= "😊"
+
+
+  
+}
+
+oyee()
